3980ec8b10e62325b114f7503b05495fa777a4ff
Lea <a@b.c>
2013-03-22 10:09:49 +0100

    +Jans Zeug

---
+++ WindowsPhoneApplication1/Verlauf.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace WindowsPhoneApplication2
+{
+    public partial class PhonePage3 : PhoneApplicationPage
+    {
+        public PhonePage3()
+        {
+            InitializeComponent();
+        }
+    }
+}
